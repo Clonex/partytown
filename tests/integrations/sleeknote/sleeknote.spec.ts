c44d4809680f92d072b3a1e9a1d02f5dfcbb92bd
Michael Salling <clonex_kontakt@hotmail.com>
2023-09-09 11:36:22 +0200

    Look for iframe for SleekNote test

--- tests/integrations/sleeknote/sleeknote.spec.ts
+++ tests/integrations/sleeknote/sleeknote.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('Sleeknote', async ({ page, browser }) => {
+test('Sleeknote', async ({ page }) => {
   await page.goto('/tests/integrations/sleeknote/');
-  await page.waitForFunction(() => document.cookie.length > 0);
 
-  expect((await browser.contexts()[0].cookies()).find((o) => o.name === 'SNS')?.value).toEqual('1');
+  const element = page.locator('iframe');
+  await element.waitFor({ state: 'hidden' });
+
+  expect(element).toBeTruthy();
 });
